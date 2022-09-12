40c15ea5b7e9b206fa3b6b2e4bbee50912ecf489
Htet Naing <htetnaing0814@gmail.com>
2022-09-12 17:44:11 +0630

    added negative

--- data.sql
+++ data.sql
@@ -5,10 +5,10 @@ INSERT INTO pets VALUES (3, 'Pikachu', DATE '2021-01-07', 1, false, 15.04);
 INSERT INTO pets VALUES (4, 'Devimon', DATE '2017-05-12', 5, true, 11);
 
 -- day 2
-INSERT INTO pets (id, name, birth_date, escape_attempts, neutered, weight_kg) VALUES (5, 'Charmander', DATE '2020-02-08', 0, false, 11);
-INSERT INTO pets (id, name, birth_date, escape_attempts, neutered, weight_kg) VALUES (6, 'Plantmon', DATE '2021-11-15', 2, true, 5.7);
-INSERT INTO pets (id, name, birth_date, escape_attempts, neutered, weight_kg) VALUES (7, 'Squirtle', DATE '1993-04-02', 3, false, 12.13);
-INSERT INTO pets (id, name, birth_date, escape_attempts, neutered, weight_kg) VALUES (8, 'Angemon', DATE '2005-06-12', 1, true, 45);
+INSERT INTO pets (id, name, birth_date, escape_attempts, neutered, weight_kg) VALUES (5, 'Charmander', DATE '2020-02-08', 0, false, -11.0);
+INSERT INTO pets (id, name, birth_date, escape_attempts, neutered, weight_kg) VALUES (6, 'Plantmon', DATE '2021-11-15', 2, true, -5.7);
+INSERT INTO pets (id, name, birth_date, escape_attempts, neutered, weight_kg) VALUES (7, 'Squirtle', DATE '1993-04-02', 3, false, -12.13);
+INSERT INTO pets (id, name, birth_date, escape_attempts, neutered, weight_kg) VALUES (8, 'Angemon', DATE '2005-06-12', 1, true, -45);
 INSERT INTO pets (id, name, birth_date, escape_attempts, neutered, weight_kg) VALUES (9, 'Boarmon', DATE '2005-06-07', 7, true, 20.4);
 INSERT INTO pets (id, name, birth_date, escape_attempts, neutered, weight_kg) VALUES (10, 'Blossom', DATE '1998-10-13', 3, true, 17);
 INSERT INTO pets (id, name, birth_date, escape_attempts, neutered, weight_kg) VALUES (11, 'Ditto', DATE '2022-05-14', 4, true, 22);
