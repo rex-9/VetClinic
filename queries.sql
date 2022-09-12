7cd3a5abb29a979ca86ed278dce9110509dcdd79
Htet Naing <htetnaing0814@gmail.com>
2022-09-12 12:04:17 +0630

    first transaction of renaming species to unspecified

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
--- day1
+-- day 1
 SELECT * from pets WHERE name LIKE '%mon';
 SELECT name from pets WHERE birth_date BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT name from pets WHERE neutered IS true and escape_attempts < 3;
@@ -7,3 +7,9 @@ SELECT name, escape_attempts from pets WHERE weight_kg > 10.5;
 SELECT * from pets WHERE neutered is true;
 SELECT * from pets WHERE name != 'Gabumon';
 SELECT * from pets WHERE weight_kg between 10.4 and 17.3;
+
+-- day 2
+BEGIN;
+ALTER TABLE pets
+RENAME COLUMN species TO unspecified;
+ROLLBACK;
